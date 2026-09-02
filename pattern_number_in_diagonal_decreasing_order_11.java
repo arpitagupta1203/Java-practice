fd3e0f48bdb204ee635c23d25cc907a263769b18
Arpita <arpitagupta1203@gmail.com>
2026-09-02 23:56:24 +0530

    Add number diagonal decreasing pattern

---
+++ pattern_number_in_diagonal_decreasing_order_11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class pattern_number_in_diagonal_decreasing_order_11 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        for (int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
